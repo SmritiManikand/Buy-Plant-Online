12a24f770cc6a6c74ea70fd84928ee888bd7fb78
user <smritimanikand@gmail.com>
2024-08-01 19:48:05 +0530

    first commit

---
+++ src/AboutUs.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import './AboutUs.css';
+
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      {}
+      <p className="about-us-description">Welcome to PlantPal , Your Ultimate Destination for Greenery!</p>
+      <p className="about-us-content">
+      At PlantPal, we believe that every home and garden deserves a touch of nature. Our online store offers a diverse collection of high-quality plants and gardening supplies, from vibrant houseplants and lush indoor greenery to stunning outdoor specimens and essential gardening tools.
+      </p>
+      {}
+      <p className="about-us-content">
+      Whether you’re a seasoned gardener or a plant enthusiast just starting out, PlantPal is here to make your plant shopping experience easy, enjoyable, and fulfilling.
+      </p>
+      {}
+
+      <p className="about-us-content">
+      Explore our curated selection of plants, find expert tips and advice, and discover unique products to nurture your green space. Join our community of plant lovers and let’s grow together!
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
